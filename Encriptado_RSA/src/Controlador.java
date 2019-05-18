10f9fbf366be8000b09e5354bd6338102146b8c7
JQuinteroC <jlqc1772@gmail.com>
2019-05-18 00:36:49 -0500

    desarrollo base de validadciones

--- Encriptado_RSA/src/Controlador.java
+++ Encriptado_RSA/src/Controlador.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -22,8 +23,13 @@ public class Controlador implements ActionListener, KeyListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(d.btnEncriptar)) {
-            d.setSize(new Dimension(d.getWidth(), d.getHeight()+ 30));
-            d.txtResultado.setSize(d.txtResultado.getWidth(), d.txtResultado.getHeight() + 30);
+            
+            if (d.txtP.getText().isEmpty() || Integer.parseInt(d.txtP.getText())< 23 || Integer.parseInt(d.txtP.getText()) > 500 ) {
+                JOptionPane.showMessageDialog(null, "pp");
+            } else {
+                d.setSize(new Dimension(d.getWidth(), d.getHeight() + 30));
+                d.txtResultado.setSize(d.txtResultado.getWidth(), d.txtResultado.getHeight() + 30);
+            }
         }
     }
 
--- Encriptado_RSA/src/Vista.java
+++ Encriptado_RSA/src/Vista.java
@@ -1,8 +1,6 @@
 
 import java.awt.Font;
 import java.awt.HeadlessException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
